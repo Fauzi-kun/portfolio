fa2718ad7165667f0ffbd026d8a2cb7f190f7002
Fauzi-kun <fauzimalik08@gmail.com>
2024-12-04 22:58:28 +0700

    Javascript <Fauzi Malik Nashrullah>

---
+++ tugas-frontend/tugas-3-javascript-dasar/looping.js
@@ -0,0 +1,12 @@
+var x = 2;
+console.log("LOOPING PERTAMA");
+while (x <= 20) {
+  console.log(x + " - I love coding");
+  x += 2;
+}
+x -= 2;
+console.log("LOOPING KEDUA");
+while (x >= 2) {
+  console.log(x + " - I will become a mobile developer");
+  x -= 2;
+}
